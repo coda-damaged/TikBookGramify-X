4008ab4a08eff02139054ac15473c49b8e20647e
Cøda <94078504+coda-damaged@users.noreply.github.com>
2024-01-30 19:28:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function validatePassword() {
+    var password = document.getElementById('passwordInput').value;
+
+    // Regular expressions for password requirements
+    var uppercaseRegex = /[A-Z]/;
+    var lowercaseRegex = /[a-z]/;
+    var digitRegex = /\d/;
+    var specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+    var invalidPasswordRegex = /password/i;
+
+    // Check password against requirements
+    var isValid = (
+        uppercaseRegex.test(password) &&
+        lowercaseRegex.test(password) &&
+        digitRegex.test(password) &&
+        specialCharRegex.test(password) &&
+        !invalidPasswordRegex.test(password)
+    );
+
+    // Display appropriate message in popup
+    if (isValid) {
+        // alert("Password is valid. Redirecting to another link...");
+        window.location.href = '/Success'
+        
+        // Add code to redirect to another link after validation
+        // Example: window.location.href = 'https://example.com';
+    } else {
+        alert("Password is missing a requirement. Please check and try again.         (Also your password can't be password nerd)");
+    }
+}
